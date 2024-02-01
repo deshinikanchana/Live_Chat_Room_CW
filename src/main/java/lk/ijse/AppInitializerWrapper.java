ad9ba10939dfa641375ab08ff47594fde4cd5a5d
deshini kanchana <deshinisampath@gmail.com>
2024-02-01 12:19:51 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/AppInitializerWrapper.java
@@ -0,0 +1,8 @@
+package lk.ijse;
+
+
+public class AppInitializerWrapper{
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
---
+++ src/main/java/lk/ijse/Controller/loginController.java
@@ -0,0 +1,63 @@
+package lk.ijse.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.server.Server;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class loginController {
+    public AnchorPane root;
+    public TextField txtUserName;
+    private Server server;
+
+    public  static List<String> names = new ArrayList<>();
+    public void initialize() {
+        new Thread(() -> {
+            try {
+                server = Server.getInstance();
+                server.makeSocket();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void btnLoginOnAction(ActionEvent event) throws IOException {
+        if(!txtUserName.getText().isEmpty()) {
+
+            names.add(txtUserName.getText());
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/View/chat_room_form.fxml"))));
+
+
+            stage.centerOnScreen();
+            stage.setTitle(txtUserName.getText() + " 's Chat Room");
+            stage.setOnCloseRequest(windowEvent -> {
+                for (String nm:names) {
+                    if(stage.getTitle().startsWith(nm)) {
+                        names.remove(nm);
+                        System.out.println(nm + " left from the chat !!!");
+                        return;
+                    }
+                }
+            });
+            stage.show();
+
+            txtUserName.setText(" ");
+
+
+        }else{
+            new Alert(Alert.AlertType.ERROR, "User Name Is Required !!!").show();
+        }
+    }
+}
